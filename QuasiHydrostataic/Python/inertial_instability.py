aed3d80fa6ab9e2a5b646be88187f57be1f70f22
francispoulin <fpoulin@uwaterloo.ca>
2021-04-19 11:43:29 -0400

    Adding a new example that solves the 1D inertial instability problem in python

---
+++ QuasiHydrostataic/Python/inertial_instability.py
@@ -0,0 +1,81 @@
+# /usr/bin/env python
+
+"""
+ Linear Stability Calculation of an inertially unstable jet using spectral methods
+
+ The basic state is that of a barotropic Bickley jet (to be generalized).
+ The perturbation is assumed to be periodic in both the zonal (x) and vertical (z) directions.
+
+"""
+import numpy as np
+from numpy import linalg as LA
+import matplotlib.pyplot as plt
+import sys
+
+from Parameters import Grid, Physics, Jet, Files
+from Parameters import Output_Parameters, save_spectrum
+
+from Plotting_scripts import plot_growth_slice, plot_growth, plot_modes_1D, plot_modes_2D
+
+### Define Parameters
+file    = Files()
+grid    = Grid(Ly = 1000e3, Lz = 3e3, Ny = 100, lat = np.pi/32)
+physics = Physics(N=1e-3, nu=0.26, kwargs={"lat": grid.lat, "NT": 1})
+jet     = Jet(kwargs={"y": grid.y, "Ly": grid.Ly, "fz": physics.fz, "fy": physics.fy})
+
+### Output Parameters 
+Output_Parameters(grid, physics, jet, file.json)
+
+### Number of eigenvalues to store and wavenumbers
+Neigs = 10                            
+dk, Nk = 1e-6, 50
+dm, Nm = 1e-4, 150
+
+ks = np.arange(0.,     Nk*dk,dk)
+ms = np.arange(dm, (Nm+1)*dm,dm)
+
+omegas = np.zeros((Nk, Nm, Neigs),              dtype=complex)
+modes  = np.zeros((Nk, Nm, Neigs, 3*grid.Ny+1), dtype=complex)
+
+### Loop over wavenumbers
+Z, I     = np.zeros((grid.Ny+1, grid.Ny+1)), np.eye(grid.Ny+1)
+Qm, Bym  = np.diag(physics.fz - jet.dU), np.diag(- physics.fy * jet.dU)
+N2       = physics.N**2
+
+for (ik, k) in enumerate(ks):
+    k2 = k**2
+    for (im, m) in enumerate(ms):
+        m2 = m**2
+
+        Um    = k*np.diag(jet.U,0) - 1j*physics.nu*m2*I
+        fyomDy = physics.fy/m*grid.Dy
+
+        # Set up eigenvalue problem: [u, v, b]
+        A = np.vstack((np.hstack((                              Um,                  (1j*Qm + fyomDy)[:,1:-1],     - 1j*k/m*I)),
+                       np.hstack(( - (1j * physics.fz * I + fyomDy)[1:-1,:],                    Um[1:-1,1:-1],   - 1/m*grid.Dy[1:-1,:])),
+                       np.hstack((                     1j*N2*k/m*I,          (N2/m*grid.Dy - 1j* Bym)[:,1:-1],             Um)) ))
+
+        # Compute eigenspectrum
+        eigVals,eigVecs = LA.eig(A)
+
+        # Sort eigenvalues
+        ind = (-np.imag(eigVals)).argsort()
+        eigVecs = eigVecs[:,ind]
+        eigVals = eigVals[ind]
+
+        # Store spectrum
+        for ie in range(Neigs):
+            omegas[ik, im, ie] = eigVals[ie]
+            modes [ik, im, ie] = eigVecs[:,ie]
+
+save_spectrum(omegas, modes, ks, ms, jet.y, Neigs, grid.Ny, file.nc)
+
+plot_growth_slice(file.nc, file.json, file.plotslicem)
+plot_growth(file.nc, file.json, file.plotgrowth)
+plot_modes_1D(file.nc, file.json, file.plotmodes1D, Neigs)
+plot_modes_2D(file.nc, file.json, file.plotmodes2D, Neigs)
+
+# To-Do
+# -> numba parallel k loop using krange!
+# -> Julia
+# -> 2D eigenvalue problem
